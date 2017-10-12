6d129774ae811c20494335ab8641087012ca1204
gantt42 <gantt42@gmail.com>
2017-10-11 20:41:47 -0400

    Added comments

--- buildtools/build.py
+++ buildtools/build.py
@@ -11,10 +11,12 @@ versionfile = "version.txt"			#relative to build.py
 
 releaseTemp = os.path.join(release, "temp")
 
+#read in version from versionFile
 with open(versionfile, 'r') as content_file:
 	version = content_file.read()
-print(version)
+print("Building " + version)
 
+#clear out or make release file
 if os.path.exists(release):
 	for root, dirs, files in os.walk(release):
 		for f in files:
@@ -23,32 +25,35 @@ if os.path.exists(release):
 			shutil.rmtree(os.path.join(root, d))
 else:
 	os.makedirs(release)
-
 os.makedirs(releaseTemp)
 
 for dirpath, dirnames, filenames in os.walk(dev):
 	for filename in [f for f in filenames if f.endswith(".pray.cos")]:
+#	Package using Monk
 		fullPathCos = os.path.join(dirpath, filename)
-		print(fullPathCos)
+		print("Packaging " + fullPathCos)
 		syatemArg = pathToMonk + " " + "\"" + fullPathCos + "\""
 		os.system(syatemArg)
 		for dirpath, dirnames, filenames in os.walk(dirpath):
 			for filename in [f for f in filenames if f.endswith(".agents")]:
+#	Moving the .agents to the releaseTemp folder
 				fullPathAgent = os.path.join(dirpath, filename)
 				newName = os.path.join(releaseTemp, filename)
-				print(newName)
+				print("Moving    " + newName)
 				if os.path.exists(newName):
 					os.remove(newName)
 				os.rename(fullPathAgent, newName)
-
+#	Copy the readme
 readmeDestination = os.path.join(releaseTemp, readme)
-print(readmeDestination)
+print("Copying   " + readmeDestination)
 if os.path.exists(readmeDestination):
 	os.remove(readmeDestination)
-print(os.path.join(dev, readme))
 shutil.copyfile(os.path.join(dev, readme), readmeDestination)
 
-shutil.make_archive(os.path.join(release, version), 'zip', releaseTemp)
+#	Zip up files
+zipFile = os.path.join(release, version)
+print("Zipping   " + zipFile + ".zip")
+shutil.make_archive(zipFile, 'zip', releaseTemp)
 shutil.rmtree(releaseTemp)
 
 print("done")
